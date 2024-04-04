54c6148a6b47b361dbc41ada7125f845148162d2
villamorbudz <L14Y08W07@citulab.net>
2024-04-04 09:50:45 +0800

    reverted to omas-as db

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php 
-	$connection = new mysqli('localhost', 'root','','dbvillamorf1');
+	$connection = new mysqli('localhost', 'root','','dbomasasf1');
 	
 	if (!$connection){
 		die (mysqli_error($mysqli));
